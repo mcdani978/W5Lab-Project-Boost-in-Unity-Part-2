822a76f68e85cb92cb145c12001b0b21666d8be1
mcdani978 <mcdani978@morrisville.edu>
2024-10-07 14:59:40 -0400

    Player controls refined for better gameplay experience

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float transportDistance = 0; // Distance to transport the player
+
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -18,24 +20,35 @@ public class CollisionHandler : MonoBehaviour
             case "Fuel":
                 Debug.Log("You picked up fuel");
                 break;
+            case "Transporter":
+                TransportPlayer();
+                break;
             default:
                 Debug.Log("Sorry, you blew up!");
                 ReloadLevel();
                 break;
-
-
         }
+    }
+
+    void TransportPlayer()
+    {
+        // Move the player a certain distance forward on the x-axis
+        Vector3 newPosition = transform.position + new Vector3(transportDistance, 0, 0);
+        transform.position = newPosition;
+        Debug.Log("Player transported to: " + newPosition);
     }
 
     void LoadNextLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
-        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
         {
-            nextSceneIndex = 0;
+            nextSceneIndex = 0; // Loop back to the first scene
         }
-        SceneManager.LoadScene(currentSceneIndex + 1);
+
+        SceneManager.LoadScene(nextSceneIndex); // Load the next scene
     }
 
     void ReloadLevel()
